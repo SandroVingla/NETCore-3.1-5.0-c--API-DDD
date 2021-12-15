20687b2bd896ae97a97642a74019ee23a8717783
SandroVingla <sanvingla@hotmail.com>
2021-12-15 13:38:00 -0300

    revisado controller e conctador no browser

--- Api.Application/Controllers/UsersController.cs
+++ Api.Application/Controllers/UsersController.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Service.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Application.Controllers{
 
-[Route("api/[comtroller]")]
+[Route("api/[controller]")]
 [ApiController]
 
 
@@ -34,5 +35,39 @@ namespace Api.Application.Controllers{
                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
            }
        }
+
+       [HttpGet]
+       [Route ("{id}", Name = "GetWithId")]
+
+       public async Task<ActionResult> Get (Guid id){
+           if(ModelState.IsValid){
+               return BadRequest(ModelState);
+           }
+           try
+           {
+             return Ok (await _service.Get (id));   
+           }
+           catch (ArgumentException e) {
+               return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+           }
+       }
+
+       [HttpPost]
+       public async Task<ActionResult> Post([FromBody] UserEntity user){
+           if(ModelState.IsValid){
+               return BadRequest(ModelState);
+           }
+           try{
+               var result = await _service.Post (user);
+               if(result != null){
+                   return Created(new Uri (Url.Link("GetWithId", new{id = result.Id})), result);
+               }else {
+                   return BadRequest();
+               }
+           }
+           catch (ArgumentException e) {
+               return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+           }
+       } 
     }
 }
